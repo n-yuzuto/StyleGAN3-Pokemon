44cdc7074dcbf4cacc3b369aca2cd2a1a8bd5908
中島柚斗 <yuto@MacBook-Air.local>
2022-04-23 19:35:59 +0900

    delete clu111

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@
 # 標準出力先
 #$-o logs/stdout_1.txt
 
-qrsh -g gcc50441 -l rt_AF=1 -l rt_F=1 -l h_rt=36:00:00 
+# qrsh -g gcc50441 -l rt_AF=1  -l h_rt=24:00:00
 
 source /etc/profile.d/modules.sh
 module load cuda/11.0/11.0.3
@@ -26,13 +26,13 @@ module load python/3.7/3.7.13
 echo "python version is"
 python3 -V
 
+git clone https://github.com/n-yuzuto/stylegan3-pokemon.git
 cd stylegan3-pokemon
 
 pip3 install -r requirements.txt
 
 echo "working directory is {$PWD}"
-# python3 dataset_tool.py --source=/home/acd14209pi/stylegan3-pokemon/pokemon_data --dest=/home/acd14209pi/stylegan3-pokemon/datasets/pokemon-256x256.zip --resolution=256x256
+python3 dataset_tool.py --source=/home/acd14209pi/stylegan3-pokemon/pokemon_data --dest=/home/acd14209pi/stylegan3-pokemon/datasets/pokemon-256x256.zip --resolution=256x256
 
 python3 check_cuda_version.py
-
 python3 train.py --outdir=/home/acd14209pi/stylegan3-pokemon/training-runs --cfg=stylegan3-t --data=/home/acd14209pi/stylegan3-pokemon/datasets/pokemon-256x256.zip --cfg=stylegan3-t --gpus=8 --batch=32 --gamma=2
