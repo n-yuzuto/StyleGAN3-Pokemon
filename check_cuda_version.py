0300407f458c6223538c84be06e23c38e7bd98dc
中島柚斗 <yuto@MacBook-Air.local>
2022-04-23 19:03:19 +0900

    add check_cuda.py

---
+++ check_cuda_version.py
@@ -0,0 +1,7 @@
+import torch
+print("pytorch information is here ------------------------")
+print("torch_version: ", torch.__version__)
+print("torch.cuda.device_count", torch.cuda.device_count())
+print("torch.cuda.get_device_name()", torch.cuda.get_device_name())
+print("torch.cuda.get_device_capability()", torch.cuda.get_device_capability())
+print("pytorch information is above------------------------")
---
+++ run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#$-l rt_AF=1
+#$-l h_rt=36:00:00
+#$-j y
+#$-cwd
+# ジョブ名
+#$-N job_name
+#$-m a
+#$-m b
+#$-m e
+#$-v GPU_COMPUTE_MODE=1
+
+
+# 標準出力先
+#$-o logs/stdout_1.txt
+
+qrsh -g gcc50441 -l rt_AF=1 -l rt_F=1 -l h_rt=36:00:00 
+
+source /etc/profile.d/modules.sh
+module load cuda/11.0/11.0.3
+module load cudnn/8.0/8.0.5
+module load gcc/11.2.0
+module load python/3.7/3.7.13
+
+echo "python version is"
+python3 -V
+
+cd stylegan3-pokemon
+
+pip3 install -r requirements.txt
+
+echo "working directory is {$PWD}"
+# python3 dataset_tool.py --source=/home/acd14209pi/stylegan3-pokemon/pokemon_data --dest=/home/acd14209pi/stylegan3-pokemon/datasets/pokemon-256x256.zip --resolution=256x256
+
+python3 check_cuda_version.py
+
+python3 train.py --outdir=/home/acd14209pi/stylegan3-pokemon/training-runs --cfg=stylegan3-t --data=/home/acd14209pi/stylegan3-pokemon/datasets/pokemon-256x256.zip --cfg=stylegan3-t --gpus=8 --batch=32 --gamma=2
