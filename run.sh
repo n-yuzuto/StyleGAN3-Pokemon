548c0177e1760021b162ef9d9d0626fa2b1cb799
中島柚斗 <yuto@macbook-air.lan>
2022-04-26 18:15:15 +0900

    f

--- run.sh
+++ run.sh
@@ -21,24 +21,21 @@ source /etc/profile.d/modules.sh
 module load cuda/11.0/11.0.3
 module load cudnn/8.0/8.0.5
 module load gcc/11.2.0
-module load python/3.10/3.10.4
+module load python/3.8/3.8.13
 
+source ~/venv_0426/bin/activate
 echo "python version is"
 python3 -V
 
-git clone https://github.com/n-yuzuto/stylegan3-pokemon.git
+# git clone https://github.com/n-yuzuto/stylegan3-pokemon.git
 cd stylegan3-pokemon
 
-wget https://github.com/ninja-build/ninja/releases/download/v1.8.2/ninja-linux.zip
-unzip unzip ninja-linux.zip -d /usr/local/bin/
-update-alternatives --install /usr/bin/ninja ninja /usr/local/bin/ninja 1 --force 
-
-pip install --upgrade pip
-pip3 install -r requirements.txt
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+pip3 install click
+# pip3 install -r requirements.txt
+# pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
 
 echo "working directory is {$PWD}"
-python3 dataset_tool.py --source=/home/acd14209pi/stylegan3-pokemon/pokemon_data --dest=/home/acd14209pi/stylegan3-pokemon/datasets/pokemon-256x256.zip --resolution=256x256
+# python3 dataset_tool.py --source=/home/acd14209pi/stylegan3-pokemon/pokemon_data --dest=/home/acd14209pi/stylegan3-pokemon/datasets/pokemon-256x256.zip --resolution=256x256
 
 python3 check_cuda_version.py
 python3 train.py --outdir=/home/acd14209pi/stylegan3-pokemon/training-runs --cfg=stylegan3-t --data=/home/acd14209pi/stylegan3-pokemon/datasets/pokemon-256x256.zip --cfg=stylegan3-t --gpus=8 --batch=32 --gamma=2
